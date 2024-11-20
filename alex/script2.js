386cff4167f1c5e0a2d29f034c222b00748a03ce
Julia Yoo <julia.mfy@gmail.com>
2024-11-20 23:10:55 +0000

    some nav changes

--- alex/script2.js
+++ alex/script2.js
@@ -2,23 +2,23 @@
 let counter = 1;
 console.log("script2.js loaded");
 
-document.addEventListener("DOMContentLoaded", function() {
-    // switchImage();
-    // setInterval(switchImage, 3000);
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     switchImage();
+//     setInterval(switchImage, 3000);
+// });
 
-// function switchImage(){
-//     if(counter > 3){
-//         counter = 1;
+function switchImage(){
+    if(counter > 3){
+        counter = 1;
         
-//     }
-//     console.log("inside changeImage", counter);
-//     for(var i = 1; i <= 3; i++){
-//     var image = "#performance" + i;
-//     document.querySelector(image).style.visibility = "hidden";
+    }
+    console.log("inside changeImage", counter);
+    for(var i = 1; i <= 3; i++){
+    var image = "#performance" + i;
+    document.querySelector(image).style.visibility = "hidden";
 
-//     }
-//     var currImage = "#performance" + counter;
-//     document.querySelector(currImage).style.visibility = "visible";
-//     counter++;
-// };
+    }
+    var currImage = "#performance" + counter;
+    document.querySelector(currImage).style.visibility = "visible";
+    counter++;
+};
